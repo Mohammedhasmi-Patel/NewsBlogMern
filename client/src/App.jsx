d5a4ce728b9c11a0b95e643b401b743a87375d4d
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-06-28 13:24:15 +0530

    install shadcn for ui styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button>Button</Button>
+    </div>
+  );
+}
 
 export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,34 @@
+// import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// //import react from "@vitejs/plugin-react-swc"; *for swc install
+// const __dirname = path.dirname("./src");
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
+
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// ✅ Proper __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
