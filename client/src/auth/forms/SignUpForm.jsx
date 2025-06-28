73d3ed13119b9eeab7cbb882a018278581b91b6a
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-06-28 16:14:02 +0530

    create signup page

--- client/src/auth/forms/SignUpForm.jsx
+++ client/src/auth/forms/SignUpForm.jsx
@@ -1,7 +1,145 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters long",
+    })
+    .max(20, {
+      message: "Username must not exceed 20 characters",
+    })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message: "Username can only contain letters, numbers, and underscores",
+    })
+    .max(50),
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+});
 
 const SignUpForm = () => {
-  return <div>signup</div>;
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl sm:max-w-5xl mx-auto flex-col md:flex-row  md:items-center gap-5">
+        {/* left part */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold text-2xl sm:text-4xl">
+            <span className="text-slate-500">Yashwi</span>
+            <span className="text-slate-900">News</span>{" "}
+          </Link>
+
+          <h2 className="text-[24px] md:text-[30px] font-bold leading-[140%] tracking-tighter pt-5 sm:pt-12">
+            Create A New Account
+          </h2>
+
+          <p className="text-slate-500 text-[14px] font-medium leading-[140%] md:text-[16px] md:font-normal mt-2">
+            Welcome to Yashwi News, Please provide your details to create an
+            account.
+          </p>
+        </div>
+
+        <div className="flex-1">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="Username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="abc@gmail.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full bg-blue-600" type="submit">
+                Submit
+              </Button>
+            </form>
+          </Form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-600">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpForm;
