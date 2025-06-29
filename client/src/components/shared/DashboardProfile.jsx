ec5d6be5e513c824569cdf79c820a563f8558d6e
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-06-29 12:52:11 +0530

    create sidebar for dashboard

---
+++ client/src/components/shared/DashboardProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardProfile = () => {
+  return (
+    <div>
+      <h2>Profile Information</h2>
+      {/* Add profile information fields here */}
+    </div>
+  );
+};
+
+export default DashboardProfile;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import BottomNavbar from "@/components/shared/BottomNavbar";
+import DashboardProfile from "@/components/shared/DashboardProfile";
+import DashboardSidebar from "@/components/shared/DashboardSidebar";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
+  const [tab, setTab] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <div>
-      <h1>Dashboard Page</h1>
+      <div className="min-h-screen flex flex-col md:flex-row w-full">
+        {/* sidebar */}
+        <div className="hidden md:block">
+          <DashboardSidebar />
+        </div>
+      </div>
+
+      <BottomNavbar />
+
+      <div>{tab == "profile" && <DashboardProfile />}</div>
     </div>
   );
 };
